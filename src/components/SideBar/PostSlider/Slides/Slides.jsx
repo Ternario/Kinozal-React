19b9a11c8fb8fe92a5b3f951197ff4c054fffdc6
ternario <ligioner123331q@gmail.com>
2020-06-24 14:58:22 +0300

    slider

--- src/components/SideBar/PostSlider/Slides/Slides.jsx
+++ src/components/SideBar/PostSlider/Slides/Slides.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import illusion from '../../../../img/illusion.jpg'
-import martian from '../../../../img/martian.jpg'
-import xmen from '../../../../img/x-men.jpg'
 
-
-const Slides = () => {
+const Slides = ({src, number}) => {
     return (
         <div className="sideBar-postSlider__slides">
             <div className="slides-item slider active">
-                <img src={illusion} alt="slide" className="slides-item__img"/>
-            </div>
-            <div className="slides-item slider">
-                <img src={martian} alt="slide" className="slides-item__img"/>
-            </div>
-            <div className="slides-item slider">
-                <img src={xmen} alt="slide" className="slides-item__img"/>
+                <img src={src} alt="slide" className="slides-item__img"/>
             </div>
         </div>
     );
